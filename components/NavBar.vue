d0d7665ff7e25e78073b98b870084bcbb94011c9
musaubrian <musaubrian15@gmail.com>
2022-12-10 21:58:24 +0300

    chore: add taliwind to project

---
+++ components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full flex justify-center items-center">
+        <div class="w-4/6 bg-gray-500">
+            <ul class="inline-flex text-lg font-medium">
+                <li>Home</li>
+                <li>search</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
