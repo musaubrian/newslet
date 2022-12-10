d53f04f530cca740aabcc1e8a4693a0a51c0d0bf
musaubrian <musaubrian15@gmail.com>
2022-12-10 22:28:36 +0300

    chore: beutify navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="w-full flex justify-center items-center">
-        <div class="w-4/6 bg-gray-500">
-            <ul class="inline-flex text-lg font-medium">
-                <li>Home</li>
-                <li>search</li>
+        <div class="w-4/6 bg-slate-300 opacity-75 rounded-2xl p-2 mt-2 shadow-md">
+            <ul class="inline-flex text-lg font-medium justify-end w-5/6">
+                <li class="px-3 py-2 hover:text-sky-500 transition-ease duration-500">
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li class="px-3 py-2 hover:text-sky-500 transition-ease duration-500">
+                    <NuxtLink to="/search">Search</NuxtLink>
+                </li>
             </ul>
         </div>
     </div>
