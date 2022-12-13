83d8413dbfae520b2c64fbd550a32a22adb408d5
musaubrian <musaubrian15@gmail.com>
2022-12-13 11:58:09 +0300

    chore: make choose category a select menu

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <img v-if="article.urlToImage" :src=article.urlToImage alt="news thumbnail" class="images">
-    <img v-else src="../assets/images/notfound.png" class="images" />
+    <!-- <img v-if="article.urlToImage" :src=article.urlToImage alt="news thumbnail" class="images"> -->
+    <!-- <img v-else src="../assets/images/notfound.png" class="images" /> -->
     <div class="px-5 my-3">
         <h1 class="
             text-gray-700 font-semibold text-xl">{{ article.title }}</h1>
         <div class="text-justify px-1 flex flex-col justify-center items-center my-2 ">
             <p class="text-lg text-gray-700">{{ article.description }}</p>
             <a :href=article.url target="_blank"
-                class="bg-sky-500 rounded-md font-semibold text-lg p-3 my-2  text-gray-100">
+                class="bg-sky-500 rounded-md font-semibold text-lg p-3 my-2  text-gray-100 cursor-pointer">
                 Keep reading
             </a>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,26 @@
 <template>
     <div class="flex flex-col items-center justify-center">
-        <div class="flex md:flex-col  bg-slate-300 mt-4 w-full md:w-5/6 rounded-lg p-3">
-            <div class="inline-flex items-center justify-around w-full">
-                <h1 class="text-gray-700 text-2xl font-bold text-center">Categories </h1>
-                <span class="material-icons cursor-pointer md:hidden" @click="isActive = !isActive">menu</span>
+        <div class="flex flex-col justify-center items-center  bg-slate-300 mt-4 w-full md:w-5/6 rounded-lg p-3">
+            <div class="inline-flex items-center justify-evenly w-full px-2">
+                <h1 class="text-gray-700 full text-2xl font-bold text-center">Category</h1>
             </div>
-            <div class="hidden md:inline-flex items-center justify-center mt-3">
-                <span class="categories" @click="updateCategory('technology')">Technology</span>
-                <span class="categories" @click="updateCategory('sports')">Sports</span>
-                <span class="categories" @click="updateCategory('business')">Business</span>
-                <span class="categories" @click="updateCategory('entertainment')">Entertainment</span>
-                <span class="categories" @click="updateCategory('health')">Health</span>
-                <span class="categories" @click="updateCategory('science')">Science</span>
+            <div class="md:w-3/6 mt-2 w-full inline-flex justify-center items-center">
+                <select name="categories" id="categories"
+                    class="ml-2 p-1 bg-transparent text-center w-full border-2 capitalize rounded-lg border-sky-600 text-xl text-gray-700 font-semibold">
+                    <option value="general" selected class="options">general</option>
+                    <option value="sports" class="options">sports</option>
+                    <option value="business" class="options">business</option>
+                    <option value="health" class="options">health</option>
+                    <option value="science" class="options">science</option>
+                    <option value="technology" class="options">technology</option>
+                    <option value="etnertainment" class="options">entertainment</option>
+                </select>
             </div>
 
         </div>
-        <div class="md:hidden flex flex-col justify-center items-center bg-slate-200 mt-2 w-full md:5/6 rounded-lg py-3 text-lg"
-            v-if="isActive">
-            <span class="categories" @click="updateCategory('technology')">Technology</span>
-            <span class="categories" @click="updateCategory('business')">Business</span>
-            <span class="categories" @click="updateCategory('entertainment')">Entertainment</span>
-            <span class="categories" @click="updateCategory('health')">Health</span>
-            <span class="categories" @click="updateCategory('science')">Science</span>
-            <span class="categories" @click="updateCategory('sports')">Sports</span>
-
+        <div v-if="pending"
+            class="text-xl font-semibold text-orange-400 text-center h-[50vh] flex items-center justify-center">
+            <h1>Be patient as we fetch your news...</h1>
         </div>
         <div class="grid md:grid-cols-2 gap-4 grid-cols-1 my-3">
             <div v-for="article in articles" :key="article.title"
@@ -39,9 +36,9 @@
 const { newsApiKey } = useRuntimeConfig();
 
 const isActive = ref(false);
-const selectedCategory = ref('general')
+let selectedCategory = ref('general')
+
 let articles = [];
-const randomKey = Math.random();
 
 const authHeaders = {
     headers: {
@@ -49,15 +46,16 @@ const authHeaders = {
     },
 };
 
+const updateCategory = (category) => {
+    selectedCategory.value = category
+    return category;
+}
+
 const uri = `https://newsapi.org/v2/top-headlines?category=${selectedCategory.value}&language=en`
 
-const { data } = await useFetch(uri, authHeaders);
+const { data, pending } = await useFetch(uri, authHeaders, { key: selectedCategory.value });
 articles = data.value.articles
 
-// const updateCategory = async (category) => {
-//     console.log("selected", category)
-//     const { data } = await useFetch(uri, authHeaders, { key: randomKey });
-//     articles = data.value.articles
-// }
+
 
 </script>
